c719e718dde0e888d1c2ce095b820249f2399eca
Alex <89657259+alextheboredape@users.noreply.github.com>
2022-08-17 12:13:16 -0700

    final changes

---
+++ scripts/deploy-escrow.js
@@ -0,0 +1,28 @@
+const { ethers } = require('hardhat');
+
+async function main() {
+	const [deployer] = await ethers.getSigners();
+
+	console.log('Deploying contracts with the account:', deployer.address);
+
+	console.log('Account balance:', (await deployer.getBalance()).toString());
+
+	if (process.env.DEVELOPMENT) {
+		console.log('local network dev mode');
+		// in development hardhat sets the block gas limit too low at only 3M
+		await ethers.provider.send('evm_setBlockGasLimit', [`0x${ethers.BigNumber.from(6000000)}`]);
+		await ethers.provider.send('evm_mine', []);
+	}
+
+	const AelinFeeEscrow = await ethers.getContractFactory('AelinFeeEscrow');
+	const aelinFeeEscrow = await AelinFeeEscrow.deploy();
+
+	console.log('AelinFeeEscrow address:', aelinFeeEscrow.address);
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
