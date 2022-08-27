4dbb57fb0e7d64a4e3bca666b40b1cdbdfbf5f7a
Tyler Quinn <ctisuruss@protonmail.com>
2022-08-27 00:08:01 -0400

    fixed testFuzzAddDelegateVouchOnlyOwner, added assume

--- test/tests/DelegateVouch.t.sol
+++ test/tests/DelegateVouch.t.sol
@@ -28,6 +28,7 @@ contract DelegateVouchTest is Test {
     }
 
     function testFuzzAddDelegateVouchOnlyOwner(address delegate, address caller) public {
+        vm.assume(caller != aelinCouncil);
         vm.prank(caller);
         vm.expectRevert("Only the contract owner may perform this action");
         DelegateVouch(delegateVouchAddress).addDelegateVouch(delegate);
@@ -46,7 +47,7 @@ contract DelegateVouchTest is Test {
     }
 
     function testFuzzRemoveDelegateVouchOnlyOwner(address delegate, address caller) public {
-        vm.assume(caller != address(aelinCouncil));
+        vm.assume(caller != aelinCouncil);
         vm.prank(caller);
         vm.expectRevert("Only the contract owner may perform this action");
         DelegateVouch(delegateVouchAddress).removeDelegateVouch(delegate);
