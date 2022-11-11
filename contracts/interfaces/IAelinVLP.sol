944f8ac1c1a4ff9ce753ac3fef456ebd51a15167
Alex <89657259+alextheboredape@users.noreply.github.com>
2022-11-11 13:54:27 -0800

    working on structs based on initial spec and contract high level functions

--- contracts/interfaces/IAelinVLP.sol
+++ contracts/interfaces/IAelinVLP.sol
@@ -4,102 +4,46 @@ pragma solidity 0.8.6;
 import {IAelinPool} from "./IAelinPool.sol";
 
 interface IAelinVLP {
-    struct UpFrontDealData {
-        string name;
-        string symbol;
-        address purchaseToken;
-        address underlyingDealToken;
-        address holder;
-        address sponsor;
-        uint256 sponsorFee;
-        bytes32 merkleRoot;
-        string ipfsHash;
-    }
-
-    struct UpFrontDealConfig {
-        uint256 underlyingDealTokenTotal;
-        uint256 purchaseTokenPerDealToken;
-        uint256 purchaseRaiseMinimum;
-        uint256 purchaseDuration;
+    struct VestingSchedule {
         uint256 vestingPeriod;
         uint256 vestingCliffPeriod;
-        bool allowDeallocation;
     }
 
-    event CreateUpFrontDeal(
-        address indexed dealAddress,
-        string name,
-        string symbol,
-        address purchaseToken,
-        address underlyingDealToken,
-        address indexed holder,
-        address indexed sponsor,
-        uint256 sponsorFee,
-        bytes32 merkleRoot,
-        string ipfsHash
-    );
+    enum Deallocation {
+        NoneAndCapped,
+        Proportional,
+        Laminar
+    }
 
-    event CreateUpFrontDealConfig(
-        address indexed dealAddress,
-        uint256 underlyingDealTokenTotal,
-        uint256 purchaseTokenPerDealToken,
-        uint256 purchaseRaiseMinimum,
-        uint256 purchaseDuration,
-        uint256 vestingPeriod,
-        uint256 vestingCliffPeriod,
-        bool allowDeallocation
-    );
+    // TBD access rules here or maybe not
+    struct AMMData {
+        address ammContract;
+        address quoteAsset;
+        address baseAsset;
+        uint256 baseAssetWeight; // NOTE only using 2 assets to start probably so can just do 100-this value for the quote asset weight
+        uint256 investorShare;
+        VestingSchedule vestingData;
+    }
 
-    event DepositDealToken(
-        address indexed underlyingDealTokenAddress,
-        address indexed depositor,
-        uint256 underlyingDealTokenAmount
-    );
+    struct LiquidityLaunch {
+        bool hasLaunchPhase;
+        uint256 initialQuotePerBase;
+        uint256 depositWindow;
+        uint256 lpFundingWindow;
+        Deallocation deallocation;
+    }
 
-    event DealFullyFunded(
-        address upFrontDealAddress,
-        uint256 timestamp,
-        uint256 purchaseExpiryTimestamp,
-        uint256 vestingCliffExpiryTimestamp,
-        uint256 vestingExpiryTimestamp
-    );
+    struct MigrationRules {
+        bool canMigrate;
+        uint256 rewardPerQuote;
+    }
 
-    event WithdrewExcess(address UpFrontDealAddress, uint256 amountWithdrawn);
-
-    event AcceptDeal(
-        address indexed user,
-        uint256 amountPurchased,
-        uint256 totalPurchased,
-        uint256 amountDealTokens,
-        uint256 totalDealTokens
-    );
-
-    event ClaimDealTokens(address indexed user, uint256 amountMinted, uint256 amountPurchasingReturned);
-
-    event SponsorClaim(address indexed sponsor, uint256 amountMinted);
-
-    event HolderClaim(
-        address indexed holder,
-        address purchaseToken,
-        uint256 amountClaimed,
-        address underlyingToken,
-        uint256 underlyingRefund,
-        uint256 timestamp
-    );
-
-    event FeeEscrowClaim(address indexed aelinFeeEscrow, address indexed underlyingTokenAddress, uint256 amount);
-
-    event ClaimedUnderlyingDealToken(address indexed user, address underlyingToken, uint256 amountClaimed);
-
-    event PoolWith721(address indexed collectionAddress, uint256 purchaseAmount, bool purchaseAmountPerToken);
-
-    event PoolWith1155(
-        address indexed collectionAddress,
-        uint256 purchaseAmount,
-        bool purchaseAmountPerToken,
-        uint256[] tokenIds,
-        uint256[] minTokensEligible
-    );
+    struct SingleRewardConfig {
+        address rewardToken;
+        uint256 rewardPerQuote;
+        VestingSchedule vestingData;
+        MigrationRules migrationRules;
+    }
 
     event SetHolder(address indexed holder);
 
