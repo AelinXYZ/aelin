df831085da624f2bb4885e5c2ff686aa864d7d06
william.ridge.95 <william.ridge.95@gmail.com>
2023-04-08 15:47:17 +1000

    setup

---
+++ test/tests/AelinNftGating.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.6;
+
+import "forge-std/Test.sol";
+import "contracts/libraries/NftCheck.sol";
+import "contracts/libraries/AelinNftGating.sol";
+
+contract AelinNftGatingTest is Test {
+    function setUp() public {}
+
+    //Test the init function
+    //Improv gas in various places
+}
