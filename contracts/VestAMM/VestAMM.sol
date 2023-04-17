52dab0f3651d5628755d8a3a66aca93e51840641
Linuz <linus@aelindao.xyz>
2023-04-17 17:49:49 +0200

    added custom errors

--- contracts/VestAMM/VestAMM.sol
+++ contracts/VestAMM/VestAMM.sol
@@ -153,11 +153,11 @@ contract VestAMM is AelinVestingToken, IVestAMM {
             SingleVestingSchedule singleVestingSchedule = vAmmInfo
                 .lpVestingSchedules[_depositTokens[i].vestingScheduleIndex]
                 .singleVestingSchedules[_depositTokens[i].singleRewardIndex];
-            Validate.holderDepositSingle(vAmmInfo.mainHolder, singleVestingSchedule.singleHolder);
-            Validate.depositSingleToken(_depositTokens[i].token, singleVestingSchedule.rewardToken, i);
+            Validate.singleHolder(vAmmInfo.mainHolder, singleVestingSchedule.singleHolder, i);
+            Validate.singleToken(_depositTokens[i].token, singleVestingSchedule.rewardToken, i);
+            Valida.singleTokenBalance(_depositTokens[i].amount, IERC20(_depositTokens[i].token).balanceOf(msg.sender), i);
+            Validate.signleDepositNotFinalized(singleVestingSchedule.finalizedDeposit, i);
 
-            require(IERC20(_depositTokens[i].token).balanceOf(msg.sender) > _depositTokens[i].amount, "not enough balance");
-            require(!singleVestingSchedule.finalizedDeposit, "deposit already finalized");
             // check deposit is not finalized here
             uint256 balanceBeforeTransfer = IERC20(_depositTokens[i].token).balanceOf(address(this));
             IERC20(_depositTokens[i].token).safeTransferFrom(msg.sender, address(this), _depositTokens[i].amount);
@@ -213,11 +213,11 @@ contract VestAMM is AelinVestingToken, IVestAMM {
         depositIncomplete
         dealOpen
     {
-        require(_lpVestingIndexList.length == _newSingleRewards.length, "arrays must be same length");
+        Validate.lpVestingAndSingleArrayLength(_lpVestingIndexList.length, _newSingleRewards.length);
         _validateSingleVestingSched(_newSingleRewards);
         for (uint8 i = 0; i < _lpVestingIndexList.length; i++) {
             LPVestingSchedule lpVestingSchedule = vAmmInfo.lpVestingSchedules[_lpVestingIndexList[i]];
-            require(lpVestingSchedule.singleVestingSchedules.length < MAX_SINGLE_REWARDS, "too many rewards already");
+            Validae.maxSingleRewards(MAX_SINGLE_REWARDS, lpVestingSchedule.singleVestingSchedules.length, i);
             lpVestingSchedule.singleVestingSchedules[lpVestingSchedule.singleVestingSchedules.length] = _newSingleRewards[i];
         }
     }
@@ -229,10 +229,7 @@ contract VestAMM is AelinVestingToken, IVestAMM {
                 _removeSingleList[i].singleRewardIndex
             ];
 
-            require(
-                msg.sender == vAmmInfo.mainHolder || singleVestingSchedule.singleHolder == msg.sender,
-                "not the right holder"
-            );
+            Validate.singleHolder(vAmmInfo.mainHolder, singleVestingSchedule.singleHolder, i);
             uint256 mainHolderAmount = holderDeposits[vAmmInfo.mainHolder][_removeSingleList[i].vestingScheduleIndex][
                 _removeSingleList[i].singleRewardIndex
             ];
@@ -291,7 +288,7 @@ contract VestAMM is AelinVestingToken, IVestAMM {
     }
 
     function depositBase() external onlyHolder depositIncomplete dealOpen {
-        require(!baseComplete, "already deposited base asset");
+        Validate.baseDepositNotCompleted(baseComplete);
         address baseAsset = ammData.baseAsset;
 
         uint256 balanceBeforeTransfer = IERC20(baseAsset).balanceOf(address(this));
@@ -312,14 +309,11 @@ contract VestAMM is AelinVestingToken, IVestAMM {
         // TODO emit any events?
         // TODO store baseAccepted under acceptDeal probably
         if (_isBase && holderDeposits[msg.sender][baseAsset] > (ammData.baseAssetAmount - baseAccepted)) {
-            require(msg.sender == vAmmInfo.mainHolder, "not the right holder");
+            Validate.mainHolder(vAmmInfo.mainHolder);
             uint256 excessAmount = holderDeposits[msg.sender][baseAsset] - (ammData.baseAssetAmount - baseAccepted);
             IERC20(ammData.baseAsset).safeTransferFrom(address(this), msg.sender, excessAmount);
         } else {
-            require(
-                msg.sender == vAmmInfo.mainHolder || singleRewards[_singleIndex].holder == msg.sender,
-                "not the right holder"
-            );
+            Validate.singleHolder(vAmmInfo.mainHolder, singleRewards[_singleIndex].singleHolder, _singleIndex);
             // TODO deleted amountClaimed from single rewards. maybe add back if needed. tbd
             uint256 excessAmount = holderDeposits[msg.sender][singleRewards[_singleIndex].token] -
                 (singleRewards[_singleIndex].rewardTokenTotal - singleRewards[_singleIndex].amountClaimed);
@@ -335,13 +329,12 @@ contract VestAMM is AelinVestingToken, IVestAMM {
     ) external lock acceptDealOpen vestingScheduleOpen(_vestingScheduleIndex, _investmentTokenAmount) {
         // TODO how to check if an array item is empty in solidity.
         // it says access to a non-existing index will throw an exception. lets test this.
-        require(!!vAmmInfo.vestingSchedule[_vestingScheduleIndex], "vesting schedule doesnt exist");
-        address investmentToken = ammContract.investmentToken;
-        require(IERC20(investmentToken).balanceOf(msg.sender) >= _investmentTokenAmount, "balance too low");
+        Validate.vestingScheduleExists(!!vAmmInfo.vestingSchedule[_vestingScheduleIndex], _vestingScheduleIndex);
+        Validate.investmentTokenBalance(_investmentTokenAmount, IERC20(ammContract.investmentToken).balanceOf(msg.sender));
         if (nftGating.hasNftList || _nftPurchaseList.length > 0) {
             AelinNftGating.purchaseDealTokensWithNft(_nftPurchaseList, nftGating, _investmentTokenAmount);
         } else if (allowList.hasAllowList) {
-            require(_investmentTokenAmount <= allowList.amountPerAddress[msg.sender], "more than allocation");
+            Validate.allocation(allowList.amountPerAddress[msg.sender], _investmentTokenAmount);
             allowList.amountPerAddress[msg.sender] -= _investmentTokenAmount;
         } else if (dealAccess.merkleRoot != 0) {
             MerkleTree.purchaseMerkleAmount(_merkleData, trackClaimed, _investmentTokenAmount, dealAccess.merkleRoot);
@@ -373,7 +366,7 @@ contract VestAMM is AelinVestingToken, IVestAMM {
         onlyHolder
         lpFundingWindow
     {
-        require(vAMMInfo.hasLiquidityLaunch, "only for new liquidity");
+        validate.notLiquidityLaunch(vAmmInfo.hasLiquidityLaunch);
         // NOTE for this method we are going to want to pass in a lot of the arguments from
         // data stored in the contract. it won't be a pure pass through like we have now
         // at this stage in the development process
@@ -383,7 +376,7 @@ contract VestAMM is AelinVestingToken, IVestAMM {
 
     // to create the pool and deposit assets after phase 0 ends
     function createLiquidity(IBalancerPool.AddLiquidity _addLiquidity) external onlyHolder lpFundingWindow {
-        require(!vAMMInfo.hasLiquidityLaunche, "only for existing liquidity");
+        Validate.isLiquidityLaunch(vAmmInfo.hasLiquidityLaunch);
         IVestAMMLibrary(ammData.ammLibrary).addLiquidity(_addLiquidity);
         finalizeVesting();
     }
@@ -415,7 +408,7 @@ contract VestAMM is AelinVestingToken, IVestAMM {
     // NOTE maybe this is a public function so we can call it internally when
     // they claim if they haven't yet withdrawn their deallocated amount
     function depositorDeallocWithdraw(uint256[] _tokenIds) external {
-        require(depositComplete && block.timestamp > lpFundingExpiry, "not time to withdraw");
+        Validate.withdrawAllowed(_depositComplete, _lpFundingExpiry);
         for (uint256 i; i < _tokenIds.length; i++) {
             VestVestingToken memory schedule = vestingDetails[_tokenId];
             // TODO need to calculate and save this number during LP submission
@@ -531,10 +524,10 @@ contract VestAMM is AelinVestingToken, IVestAMM {
         ClaimType _claimType,
         uint256 _claimIndex
     ) internal {
-        require(ownerOf(_tokenId) == msg.sender, "must be owner to claim");
+        Validate.owner(ownerOf(_tokenId));
         // TODO double check this doesn't error if there are no single sided rewards
         uint256 claimableAmount = claimableTokens(_tokenId, _claimType, _claimIndex);
-        require(claimableAmount > 0, "no lp tokens ready to claim");
+        Validate.claimBalance(claimableAmount);
         if (_claimType == ClaimType.LP) {
             vestingDetails[_tokenId].lastClaimedAt = block.timestamp;
             totalLPClaimed += claimableAmount;
@@ -558,12 +551,12 @@ contract VestAMM is AelinVestingToken, IVestAMM {
     function _validateSchedules(LPVestingSchedule[] _vestingSchedules) internal {
         Validate.maxVestingPeriods(MAX_LP_VESTING_SCHEDULES, _vestingSchedules.length);
         for (uint256 i; i < _vestingSchedules.length; ++i) {
-            Validate.vestingCliff(1825 days, _vestingSchedules[i].schedule.vestingCliffPeriod);
-            Validate.vestingPeriod(1825 days, _vestingSchedules[i].schedule.vestingPeriod);
-            Validate.investorShare(100 * 10**18, 0, _vestingSchedules[i].investorLPShare);
-            Validate.hasTotalBaseTokens(_vestingSchedules[i].totalBaseTokens);
-            Validate.nothingClaimed(_vestingSchedules[i].claimed);
-            Validate.maxSingleRewards(MAX_SINGLE_REWARDS, _vestingSchedules[i].singleVestingSchedules.length);
+            Validate.vestingCliff(1825 days, _vestingSchedules[i].schedule.vestingCliffPeriod, i);
+            Validate.vestingPeriod(1825 days, _vestingSchedules[i].schedule.vestingPeriod, i);
+            Validate.investorShare(100 * 10**18, 0, _vestingSchedules[i].investorLPShare, i);
+            Validate.hasTotalBaseTokens(_vestingSchedules[i].totalBaseTokens, i);
+            Validate.nothingClaimed(_vestingSchedules[i].claimed, i);
+            Validate.maxSingleRewards(MAX_SINGLE_REWARDS, _vestingSchedules[i].singleVestingSchedules.length, i);
             _validateSingleVestingSched(_vestingSchedules[i].singleVestingSchedules);
         }
     }
@@ -639,7 +632,7 @@ contract VestAMM is AelinVestingToken, IVestAMM {
     }
 
     modifier dealOpen() {
-        require(!isCancelled, "deal is cancelled");
+        Validate.daelIsOpen(!isCancelled);
         _;
     }
 
@@ -670,11 +663,12 @@ contract VestAMM is AelinVestingToken, IVestAMM {
                 break;
             }
         }
-        require(
-            depositedPerVestSchedule[_vestingScheduleIndex] + _investmentTokenAmount <=
-                maxInvTokensPerVestSchedule[_vestingScheduleIndex] ||
-                (otherBucketsFull && vAmmInfo.deallocation == Deallocation.Proportional),
-            "purchased more than total"
+        Validate.purchaseAmount(
+            depositedPerVestSchedule[_vestingScheduleIndex],
+            maxInvTokensPerVestSchedule[_vestingScheduleIndex],
+            _investmentTokenAmount,
+            otherBucketsFull,
+            vAmmInfo.deallocation == Deallocation.Proportional
         );
         _;
     }
--- contracts/libraries/validation/VestAMMValidation.sol
+++ contracts/libraries/validation/VestAMMValidation.sol
@@ -10,26 +10,47 @@ library Validate {
     error VestAMMNotCancelled(bool isCancelled, uint256 lpDepositTime, uint256 lpFundingExpiry);
     error VestAMMNotInFundingWindow(bool depositCompleted, bool beforeDepositExpiry, bool afterLpFundingExpiry);
     error VestAMMNotInDepositWindow(bool depositCompleted, bool afterDepositExpiry);
-    error VestingCliffTooLong(uint256 allowed, uint256 actual);
-    error VestingPeriodTooLong(uint256 allowed, uint256 actual);
-    error InvalidInvestorShare(uint256 maxAllowed, uint256 minAllowed, uint256 actual);
+    error VestingCliffTooLong(uint256 allowed, uint256 actual, uint256 index);
+    error VestingPeriodTooLong(uint256 allowed, uint256 actual, uint256 index);
+    error InvalidInvestorShare(uint256 maxAllowed, uint256 minAllowed, uint256 actual, uint256 index);
     error SingleVestingCliffTooLong(uint256 allowed, uint256 actual, uint256 index);
     error SingleVestingPeriodTooLong(uint256 allowed, uint256 actual, uint256 index);
     error SingleHolderAddressNull();
     error SingleTokensNotAllocated(uint256 index);
     error SingleClaimedNotZero(uint256 index);
     error SingleDepositNotCompleted(uint256 index);
-    error BaseTokensNotAllocated();
-    error TooManySingleRewards(uint256 allowed, uint256 actual);
+    error BaseTokensNotAllocated(uint256 index);
+    error TooManySingleRewards(uint256 allowed, uint256 actual, uint256 index);
     error TooManyVestingPeriods(uint256 allowed, uint256 actual);
     error AMMPoolDoesNotExist(address poolAddress);
-    error ClaimedNotZero();
+    error ClaimedNotZero(uint256 index);
     error AllowListAndNFTListNotAllowed();
     error AllowListAndMerkleNotAllowed();
     error NFTListAndMerkleNotAllowed();
     error IPFSHashNotProvided();
-    error InvalidHolderDepositSinlge(address mainHolder, address singleHolder, address caller);
+    error InvalidSinlgeHolder(address mainHolder, address singleHolder, address caller, uint256 index);
     error InvalidDepositSingleToken(address expected, address actual, uint256 index);
+    error InsufficientSingleTokenBalance(uint256 expected, uint256 actual, uint256 index);
+    error SingleDepositCompleted(uint256 index);
+    error LpVestingAndSingleLengthsDoNotMatch(uint256 lpVestingArrayLength, uint256 singleArrayLength);
+    error BaseAlreadyDeposited();
+    error InvalidMainHolder(address mainHolder, address caller);
+    error VestingScheduleDoesNotExist(uint256 index);
+    error InsufficientInvestmentTokenBalance(uint256 expected, uint256 actual);
+    error MoreThanAllocation(uint256 expected, uint256 actual);
+    error LiquidityLaunchNotAllowed();
+    error OnlyLiquidityLaunchAllowed();
+    error WithdrawNotAllowed(bool depositComplete, uint256 lpFundingExpiry);
+    error OnlyOwnerCanClaim(address owner, address caller);
+    error NothingToClaim();
+    error DealCancelled();
+    error PurchaseAmountExceedsMax(
+        uint256 alreadyPurchased,
+        uint256 purchaseAmount,
+        uint256 maxPurchaseAmount,
+        bool bucketsFull,
+        bool proportionalDeallocation
+    );
 
     function singleHolderNotNull(address _address) external pure {
         if (_address == address(0)) {
@@ -111,15 +132,23 @@ library Validate {
         }
     }
 
-    function vestingCliff(uint256 _allowed, uint256 _actual) external pure {
+    function vestingCliff(
+        uint256 _allowed,
+        uint256 _actual,
+        uint256 _index
+    ) external pure {
         if (_actual > _allowed) {
-            revert VestingCliffTooLong({allowed: _allowed, actual: _actual});
+            revert VestingCliffTooLong({allowed: _allowed, actual: _actual, index: _index});
         }
     }
 
-    function vestingPeriod(uint256 _allowed, uint256 _actual) external pure {
+    function vestingPeriod(
+        uint256 _allowed,
+        uint256 _actual,
+        uint256 _index
+    ) external pure {
         if (_actual > _allowed) {
-            revert VestingPeriodTooLong({allowed: _allowed, actual: _actual});
+            revert VestingPeriodTooLong({allowed: _allowed, actual: _actual, index: _index});
         }
     }
 
@@ -146,16 +175,17 @@ library Validate {
     function investorShare(
         uint256 _maxAllowed,
         uint256 _minAllowed,
-        uint256 _actual
+        uint256 _actual,
+        uint256 _index
     ) external pure {
         if (_actual > _maxAllowed || _actual <= _minAllowed) {
-            revert InvalidInvestorShare({maxAllowed: _maxAllowed, minAllowed: _minAllowed, actual: _actual});
+            revert InvalidInvestorShare({maxAllowed: _maxAllowed, minAllowed: _minAllowed, actual: _actual, index: _index});
         }
     }
 
-    function hasTotalBaseTokens(uint256 _totalBaseTokens) external pure {
+    function hasTotalBaseTokens(uint256 _totalBaseTokens, uint256 _index) external pure {
         if (_totalBaseTokens == 0) {
-            revert BaseTokensNotAllocated();
+            revert BaseTokensNotAllocated({index: _index});
         }
     }
 
@@ -165,9 +195,9 @@ library Validate {
         }
     }
 
-    function nothingClaimed(uint256 _claimed) external pure {
+    function nothingClaimed(uint256 _claimed, uint256 _index) external pure {
         if (_claimed > 0) {
-            revert ClaimedNotZero();
+            revert ClaimedNotZero({index: _index});
         }
     }
 
@@ -177,9 +207,13 @@ library Validate {
         }
     }
 
-    function maxSingleReward(uint256 _allowed, uint256 _actual) external pure {
+    function maxSingleReward(
+        uint256 _allowed,
+        uint256 _actual,
+        uint256 _index
+    ) external pure {
         if (_actual > _allowed) {
-            revert TooManySingleRewards({allowed: _allowed, actual: _actual});
+            revert TooManySingleRewards({allowed: _allowed, actual: _actual, index: _index});
         }
     }
 
@@ -225,19 +259,137 @@ library Validate {
         }
     }
 
-    function holderDepositSingle(address _mainHolder, address _singleHolder) external view {
+    function singleHolder(
+        address _mainHolder,
+        address _singleHolder,
+        uint256 _index
+    ) external view {
         if (_mainHolder != msg.sender && _singleHolder != msg.sender) {
-            revert InvalidHolderDepositSinlge({mainHolder: _mainHolder, singleHolder: _singleHolder, caller: msg.sender});
+            revert InvalidSinlgeHolder({
+                mainHolder: _mainHolder,
+                singleHolder: _singleHolder,
+                caller: msg.sender,
+                index: _index
+            });
         }
     }
 
-    function depositSingleToken(
+    function singleToken(
         address _expected,
         address _actual,
         uint256 _index
     ) external pure {
         if (_expected != _actual) {
             revert InvalidDepositSingleToken({expected: _expected, actual: _actual, index: _index});
+        }
+    }
+
+    function singleTokenBalance(
+        uint256 _expected,
+        uint256 _actual,
+        uint256 _index
+    ) external pure {
+        if (_actual < _expected) {
+            revert InsufficientSingleTokenBalance({expected: _expected, actual: _actual, index: _index});
+        }
+    }
+
+    function singleDepositNotFinalized(bool _depositFinalized, uint256 _index) external pure {
+        if (_depositFinalized) {
+            revert SingleDepositCompleted({index: _index});
+        }
+    }
+
+    function lpVestingAndSingleArrayLength(uint256 _lpVestingArrayLength, uint256 _singleArrayLength) external pure {
+        if (_lpVestingArrayLength != _singleArrayLength) {
+            revert LpVestingAndSingleLengthsDoNotMatch({
+                lpVestingArrayLength: _lpVestingArrayLength,
+                singleArrayLength: _singleArrayLength
+            });
+        }
+    }
+
+    function baseDepositNotCompleted(bool _baseDeposited) external pure {
+        if (_baseDeposited) {
+            revert BaseAlreadyDeposited();
+        }
+    }
+
+    function mainHolder(address _mainHolder) external view {
+        if (_mainHolder != msg.sender) {
+            revert InvalidMainHolder({mainHolder: _mainHolder, caller: msg.sender});
+        }
+    }
+
+    function validateVestingScheduleExists(bool _exists, uint256 _index) external pure {
+        if (!_exists) {
+            revert VestingScheduleDoesNotExist({index: _index});
+        }
+    }
+
+    function validateInvestmentTokenBalance(uint256 _expected, uint256 _actual) external pure {
+        if (_actual < _expected) {
+            revert InsufficientInvestmentTokenBalance({expected: _expected, actual: _actual});
+        }
+    }
+
+    function allocation(uint256 expected, uint256 actual) external pure {
+        if (actual > expected) {
+            revert MoreThanAllocation({expected: expected, actual: actual});
+        }
+    }
+
+    function notLiquidityLaunch(bool _isLiquidityLaunch) external pure {
+        if (_isLiquidityLaunch) {
+            revert LiquidityLaunchNotAllowed();
+        }
+    }
+
+    function isLiquidityLaunch(bool _isLiquidityLaunch) external pure {
+        if (!_isLiquidityLaunch) {
+            revert OnlyLiquidityLaunchAllowed();
+        }
+    }
+
+    function withdrawAllowed(bool _depositComplete, uint256 _lpFundingExpiry) external view {
+        if (!_depositComplete || block.timestamp <= _lpFundingExpiry) {
+            revert WithdrawNotAllowed({depositComplete: _depositComplete, lpFundingExpiry: _lpFundingExpiry});
+        }
+    }
+
+    function owner(address _owner) external view {
+        if (_owner != msg.sender) {
+            revert OnlyOwnerCanClaim({owner: _owner, caller: msg.sender});
+        }
+    }
+
+    function hasClaimBalance(uint256 _balance) external pure {
+        if (_balance == 0) {
+            revert NothingToClaim();
+        }
+    }
+
+    function dealIsOpen(bool _dealOpen) external pure {
+        if (!_dealOpen) {
+            revert DealCancelled();
+        }
+    }
+
+    function purchaseAmount(
+        uint256 _alreadyPurchased,
+        uint256 _purchaseAmount,
+        uint256 _maxPurchaseAmount,
+        bool _bucketsFull,
+        bool _proporionalDeallocation
+    ) external pure {
+        if (_alreadyPurchased + _purchaseAmount > _maxPurchaseAmount && !_bucketsFull && !_proporionalDeallocation) {
+            revert PurchaseAmountExceedsMax({
+                alreadyPurchased: _alreadyPurchased,
+                purchaseAmount: _purchaseAmount,
+                maxPurchaseAmount: _maxPurchaseAmount,
+                bucketsFull: _bucketsFull,
+                proportionalDeallocation: _proporionalDeallocation
+            });
         }
     }
 }
