e4021263ef08786d2903790eb3e0fd5f2d515ae4
0xcdb <cb0x@protonmail.com>
2023-08-15 20:25:47 +0200

    Update SwapNFT.sol

--- contracts/SwapNFT.sol
+++ contracts/SwapNFT.sol
@@ -2,23 +2,17 @@
 pragma solidity 0.8.6;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import {ISwapNFT} from "./interfaces/ISwapNFT.sol";
 
 contract SwapNFT is ERC721, ISwapNFT {
-    address public immutable aelin;
-
     uint256 public tokenCount;
 
     mapping(address => bool) public hasMinted;
 
-    constructor(address _aelinAddress) ERC721("AelinSwapNFT", "SwapNFT") {
-        aelin = _aelinAddress;
-    }
+    constructor() ERC721("AelinSwapNFT", "SwapNFT") {}
 
     function mint() external {
-        if (IERC20(aelin).balanceOf(msg.sender) == 0) revert BalanceIsZero();
         if (hasMinted[msg.sender]) revert CannotMintTwice();
 
         emit SwapNFTMinted(msg.sender, tokenCount);
