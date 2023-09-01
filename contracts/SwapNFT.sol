80137c63283e10054a45df6070f405397775b317
0xcdb <cb0x@protonmail.com>
2023-09-01 12:27:26 +0200

    Update SwapNFT.sol

--- contracts/SwapNFT.sol
+++ contracts/SwapNFT.sol
@@ -7,6 +7,8 @@ import {ISwapNFT} from "./interfaces/ISwapNFT.sol";
 
 contract SwapNFT is ERC721, ISwapNFT {
     uint256 public tokenCount;
+    string public constant BASE_URI =
+        "https://v2.akord.com/public/vaults/active/2KzfEiio_umg2tFNymCwSp6qt7Yu4phwvqlsQ1b9u4s/gallery#public/df629071-3d77-4538-af9f-c9a6df2897dc";
 
     mapping(address => bool) public hasMinted;
 
@@ -20,6 +22,14 @@ contract SwapNFT is ERC721, ISwapNFT {
         _mint(msg.sender, tokenCount);
         hasMinted[msg.sender] = true;
         tokenCount += 1;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return BASE_URI;
+    }
+
+    function tokenURI(uint256) public pure override returns (string memory) {
+        return _baseURI();
     }
 
     function transferFrom(address, address, uint256) public pure override {
